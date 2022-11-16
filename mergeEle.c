648f930efccec446d04c397db850305e3e7e06e0
xiangliang-zhang <981049125@qq.com>
2022-11-16 17:25:23 +0800

    三指针尾插合并两个有序数组

---
+++ mergeEle.c
@@ -0,0 +1,25 @@
+#include <math.h>
+#include <stdio.h>
+#include "stdlib.h"
+
+// 三指针尾插法，优点：省空间
+void merge(int *nums1, int nums1Size, int m, int *nums2, int nums2Size, int n) {
+
+    int a = nums1Size - 1, i = m - 1, j = n - 1;
+    while (i >= 0 && j >= 0) {
+        if (nums1[i] <= nums2[j]) nums1[a--] = nums2[j--];
+        else nums1[a--] = nums1[i--];
+    }
+
+    while (i >= 0) nums1[a--] = nums1[i--];
+    while (j >= 0) nums1[a--] = nums2[j--];
+}
+
+//int main() {
+//    int nums1[] = {4, 0, 0, 0, 0, 0};
+//    int nums2[] = {1, 2, 3, 5, 6};
+//    merge(nums1, 6, 1, nums2, 5, 5);
+//    for (int i = 0; i < 6; i++) {
+//        printf("%d\t", nums1[i]);
+//    }
+//}
