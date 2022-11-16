7f36908e33b69b0c9814c19b147543d7e95f3495
xiangliang-zhang <981049125@qq.com>
2022-11-16 14:57:16 +0800

    使用qsort()排序方法进行重复元素的检测

--- repeat_ele_sort.c
+++ repeat_ele_sort.c
@@ -1,35 +1,27 @@
-#include <stdbool.h>
 #include <stdlib.h>
-#include "stdio.h"
+#include <stdio.h>
+#include "stdbool.h"
 
-// 额外的库需要下载，并复制到include文件夹内
-#include <uthash.h>
+// 这里的参数为const void*, 意味着可以传入任何类型的参数, 然后将其转换成int*类型, 然后加*取其值
+int cmp(const void *_a, const void *_b) {
+    char a = *(char *) _a, b = *(char *) _b;
+    return a - b;
+}
 
-typedef struct hashTable {
-    int key;
-    UT_hash_handle hh;
-} hash_table;
-
+// qsort存在于stdlib.h库, 参数分别为要排序的数组、数组的大小、数组元素的大小、比较方法
 bool containsDuplicate(int *nums, int numsSize) {
-    hash_table *set = NULL;
-    for (int i = 0; i < numsSize; i++) {
-        hash_table *tmp;
-        HASH_FIND_INT(set, nums + i, tmp); // 参数1为要寻找的哈希表， 参数2为变量的地址, 参数3为寻找到的变量的地址
-        if (tmp == NULL) {
-            tmp = malloc(sizeof(hash_table));
-            tmp->key = nums[i];
-            HASH_ADD_INT(set, key, tmp);
-        } else {
+    qsort(nums, numsSize, sizeof(int), cmp);
+    for (int i = 0; i < numsSize - 1; i++) {
+        if (nums[i] == nums[i + 1]) {
             return true;
         }
     }
     return false;
 }
 
-
 int main() {
-    int nums[] = {1, 1, 2, 3, 4, 5};
+    int nums[] = {1, 3, 2, 6, 7, 8, 9, 10, 5};
     int numsSize = sizeof(nums) / sizeof(nums[0]);
-    int res = containsDuplicate(&nums, numsSize);
+    int res = containsDuplicate(nums, numsSize);
     printf("%d", res);
 }
