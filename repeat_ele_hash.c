ac88659961d8d942fd4661bcf7e0b8d29912a49b
xiangliang-zhang <981049125@qq.com>
2022-11-16 10:40:53 +0800

    使用qsort()排序方法进行重复元素的检测

--- repeat_ele_hash.c
+++ repeat_ele_hash.c
@@ -3,8 +3,8 @@
 #include "stdbool.h"
 
 // 这里的参数为const void*, 意味着可以传入任何类型的参数, 然后将其转换成int*类型, 然后加*取其值
-int cmp(const void* _a, const void* _b) {
-    char a = *(char*)_a, b = *(char*)_b;
+int cmp(const void *_a, const void *_b) {
+    char a = *(char *) _a, b = *(char *) _b;
     return a - b;
 }
 
@@ -19,8 +19,8 @@ bool containsDuplicate(int *nums, int numsSize) {
     return false;
 }
 
-int main(){
-    int nums[] = {1, 3, 2, 6, 7, 8};
+int main() {
+    int nums[] = {1, 3, 2, 6, 7, 8, 9, 10};
     int numsSize = sizeof(nums) / sizeof(nums[0]);
     int res = containsDuplicate(nums, numsSize);
     printf("%d", res);
