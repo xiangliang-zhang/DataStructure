0ea865d1b0ca5350c34d7315864ae43c07865688
xiangliang-zhang <981049125@qq.com>
2022-11-22 00:03:39 +0800

    判断两个字符串是否为异位， 哈希

---
+++ isAnagram.py
@@ -0,0 +1,22 @@
+import collections
+
+# 判断两个字符串是否为异位
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_dict = collections.defaultdict(int)
+        for i in s:
+            s_dict[i] += 1
+        for i in t:
+            if i not in s:
+                return False
+            else:
+                s_dict[i] -= 1
+        for i in s_dict.values():
+            if i != 0:
+                return False
+        return True
+
+s = Solution()
+str1 = "rat"
+str2 = "car"
+print(s.isAnagram(str1, str2))
