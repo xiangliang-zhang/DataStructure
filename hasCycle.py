f5f2e806762568a3a0430ded525e0db9d506261b
xiangliang-zhang <981049125@qq.com>
2022-11-21 23:49:40 +0800

    判断单链表是否有循环，快慢指针

---
+++ hasCycle.py
@@ -0,0 +1,41 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+# 快慢指针，慢走1，快走2
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow = head
+        fast = head
+        while slow != None and fast != None:
+            slow = slow.next
+            if fast.next:
+                fast = fast.next.next
+            else:
+                break
+            if slow == fast:
+                return True
+        return False
+
+
+node_list = [3, 2, 0, -4, 5]
+l1 = ListNode(3)
+l2 = ListNode(2)
+l3 = ListNode(0)
+l4 = ListNode(-4)
+l5 = ListNode(5)
+
+l1.next = l2
+l2.next = l3
+l3.next = l4
+l4.next = l5
+# l4.next = l2
+
+
+s = Solution()
+print(s.hasCycle(l1))
