6cac45ea13505cee71eaf95aa759a9302efe49a5
xiangliang-zhang <981049125@qq.com>
2022-11-22 00:00:23 +0800

    杨辉三角，动态规划

---
+++ generate_triangle.py
@@ -0,0 +1,19 @@
+from typing import List
+
+# 杨辉三角，动态规划
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        res = []
+        for i in range(1, numRows + 1):
+            res.append([0] * i)
+        for i in range(len(res)):
+            for j in range(len(res[i])):
+                if i == 0 or j == 0 or j == i:
+                    res[i][j] = 1
+                else:
+                    res[i][j] = res[i - 1][j - 1] + res[i - 1][j]
+        return res
+
+
+a = Solution()
+print(a.generate(0))
